d02987f877f0030e8d495928e4829d29fd07c0e9
Brad Parbs <brad@bradparbs.com>
2022-10-25 15:14:53 -0500

    Update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,27 +2,22 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="w-full px-4 mx-auto mt-6 max-w-container sm:px-6 lg:px-8">
-      <div className="py-10">
-        <p className="mt-3 text-sm leading-6 text-center text-slate-500">
-          <span className="text-2xl font-extrabold tracking-tight text-sky-500 hover:text-sky-600">
-            <Link href="/">
-							mouse.rip
-            </Link>
-          </span>
+    <footer class="mx-auto mt-32 w-full max-w-container px-4 sm:px-6 lg:px-8">
+      <div class="border-t border-slate-900/5 py-10">
+        <p class="mt-5 text-center text-2xl tracking-tight font-extralight text-sky-500">
+          <Link href="/">
+            mouse.rip
+          </Link>
         </p>
-        <div className="flex items-center justify-center mt-1 space-x-4 text-sm font-semibold leading-6 text-slate-700">
-          <span className="text-sky-800 hover:text-sky-400">
-            <Link href="https://github.com/MHCommunity/mouse.rip">
-							GitHub
-            </Link>
-          </span>
-          <div className="w-px h-4 bg-slate-500/20" />
-          <span className="text-sky-800 hover:text-sky-400">
-            <Link href="https://discordapp.com/invite/Ya9zEdk">
-							Discord
-            </Link>
-          </span>
+        <div class="mt-2 flex items-center justify-center space-x-2 text-sm font-light leading-6 text-sky-600">
+          <Link href="https://github.com/MHCommunity/mouse.rip">
+            GitHub
+          </Link>
+          <div class="h-4 w-px bg-slate-500/20">
+          </div>
+          <Link href="https://discordapp.com/invite/Ya9zEdk">
+            Discord
+          </Link>
         </div>
       </div>
     </footer>
