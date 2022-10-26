95668feae006376ea0084e6b407b14e289f4268e
Brad Parbs <brad@bradparbs.com>
2022-10-26 11:55:31 -0500

    Switch to CF functions

---
+++ pages/api/functions/submit-challenge-entry.js
@@ -0,0 +1,57 @@
+const { Client } = require('@notionhq/client')
+
+const notion = new Client({
+  auth: process.env.INTEGRATION_NOTION_API_KEY,
+})
+
+export async function onRequestPost({ request }) {
+  try {
+    const { challengeType, hunterId, discordId } = JSON.parse(request.body)
+
+    await notion.pages.create({
+      parent: {
+        database_id: process.env.INTEGRATION_NOTION_DATABASE_ID,
+      },
+      properties: {
+        HunterID: {
+          title: [
+            {
+              text: {
+                content: hunterId,
+              },
+            },
+          ],
+        },
+        DiscordID: {
+          rich_text: [
+            {
+              text: {
+                content: discordId,
+              },
+            },
+          ],
+        },
+        Challenge: {
+          rich_text: [
+            {
+              text: {
+                content: challengeType,
+              },
+            },
+          ],
+        },
+      },
+    })
+
+    return new Response(
+      JSON.stringify({ success: true, message: 'You have successfully entered the challenge.' }),
+      { status: 200, headers: { 'Content-Type': 'application/json' } }
+    )
+
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ success: false, message: 'There was an error submitting your entry.', error: error.message }),
+      { status: 500, headers: { 'Content-Type': 'application/json' } }
+    )
+  }
+}
