aeffe7725b2180f7a3b5109ded78be2f34abb4a8
Brad Parbs <brad@bradparbs.com>
2022-10-26 11:48:21 -0500

    Switch to CF functions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  experimental: { runtime: 'experimental-edge' },
   reactStrictMode: false,
   swcMinify: true,
   images: { unoptimized: true },
