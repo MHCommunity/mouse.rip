4c52d17f65b1f2b9c720401c63e240884bd7d698
Brad Parbs <brad@bradparbs.com>
2022-10-26 15:59:42 -0500

    Debug live because wrangler is being silly

--- functions/api/submit-challenge-entry.js
+++ functions/api/submit-challenge-entry.js
@@ -1,55 +1,15 @@
 const { Client } = require('@notionhq/client')
 
-export async function onRequestPost({ request, env }) {
-  const notion = new Client({
-    auth: env.INTEGRATION_NOTION_API_KEY,
-  })
-
+export async function onRequestPost(context) {
   try {
-    await notion.pages.create({
-      parent: {
-        database_id: env.INTEGRATION_NOTION_DATABASE_ID,
+    let input = await context.request.formData();
+    let pretty = JSON.stringify([...input], null, 2);
+    return new Response(pretty, {
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
       },
-      properties: {
-        HunterID: {
-          title: [
-            {
-              text: {
-                content: request.body.hunterId,
-              },
-            },
-          ],
-        },
-        DiscordID: {
-          rich_text: [
-            {
-              text: {
-                content: request.body.discordId,
-              },
-            },
-          ],
-        },
-        Challenge: {
-          rich_text: [
-            {
-              text: {
-                content: request.body.challengeType,
-              },
-            },
-          ],
-        },
-      },
-    })
-
-    return new Response(
-      JSON.stringify({ success: true, message: 'You have successfully entered the challenge.' }),
-      { status: 200, headers: { 'Content-Type': 'application/json' } }
-    )
-
-  } catch (error) {
-    return new Response(
-      JSON.stringify({ success: false, message: 'There was an error submitting your entry.', error: error.message }),
-      { status: 500, headers: { 'Content-Type': 'application/json' } }
-    )
+    });
+  } catch (err) {
+    return new Response('Error parsing JSON content', { status: 400 });
   }
 }
