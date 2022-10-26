0f5026f1a22b3452cf07d3b42e36f18a0bcc3afa
Brad Parbs <brad@bradparbs.com>
2022-10-26 16:03:45 -0500

    Debug live because wrangler is being silly

--- functions/api/submit-challenge-entry.js
+++ functions/api/submit-challenge-entry.js
@@ -1,15 +1,57 @@
 const { Client } = require('@notionhq/client')
 
-export async function onRequestPost(context) {
+export async function onRequestPost({ request, env }) {
+  const notion = new Client({
+    auth: env.INTEGRATION_NOTION_API_KEY,
+  })
+
+  const { challengeType, hunterId, discordId } = JSON.parse(request.body)
+
   try {
-    let input = await context.request.formData();
-    let pretty = JSON.stringify([...input], null, 2);
-    return new Response(pretty, {
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
+    await notion.pages.create({
+      parent: {
+        database_id: env.INTEGRATION_NOTION_DATABASE_ID,
       },
-    });
-  } catch (err) {
-    return new Response('Error parsing JSON content', { status: 400 });
+      properties: {
+        HunterID: {
+          title: [
+            {
+              text: {
+                content: hunterId,
+              },
+            },
+          ],
+        },
+        DiscordID: {
+          rich_text: [
+            {
+              text: {
+                content: discordId,
+              },
+            },
+          ],
+        },
+        Challenge: {
+          rich_text: [
+            {
+              text: {
+                content: challengeType,
+              },
+            },
+          ],
+        },
+      },
+    })
+
+    return new Response(
+      JSON.stringify({ success: true, message: 'You have successfully entered the challenge.' }),
+      { status: 200, headers: { 'Content-Type': 'application/json' } }
+    )
+
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ success: false, message: 'There was an error submitting your entry.', error: error.message }),
+      { status: 500, headers: { 'Content-Type': 'application/json' } }
+    )
   }
 }
