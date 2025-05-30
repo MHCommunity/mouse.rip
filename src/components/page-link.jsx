40f066e0163fcd70657467c74b26236426a5206b
Brad Parbs <brad@bradparbs.com>
2025-05-30 15:13:20 -0500

    update page link

--- src/components/page-link.jsx
+++ src/components/page-link.jsx
@@ -1,8 +1,12 @@
+import clsx from 'clsx';
 import Link from 'next/link';
 
-export function PageLink({ href, children }) {
+export function PageLink({ href, className, children }) {
   return (
-    <Link href={href} className="text-sky-600 dark:text-sky-200 hover:text-sky-700 dark:hover:text-sky-300">
+    <Link href={href} className={clsx(
+      className,
+      'text-pink-600 dark:text-pink-400 hover:text-pink-800 dark:hover:text-pink-200 transition-colors',
+    )}>
       {children}
     </Link>
   );
